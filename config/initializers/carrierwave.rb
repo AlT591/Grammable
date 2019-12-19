cef0fe73f5a091c3f820959a628e84cf809c3d54
Al Trujillo <Trujillo0525@gmail.com>
2019-12-19 03:56:19 +0000

    added AWS keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,10 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV.get("AWS_ACCESS_KEY"),        # required
-    aws_secret_access_key: ENV.get("AWS_SECRET_KEY"),        # required
+    aws_access_key_id:     ENV.fetch("AWS_ACCESS_KEY"),        # required
+    aws_secret_access_key: ENV.fetch("AWS_SECRET_KEY"),        # required
   }
-  config.fog_directory  = ENV.get("AWS_BUCKET")              # required
+  config.fog_directory  = ENV.fetch("AWS_BUCKET")              # required
 end
 
 
